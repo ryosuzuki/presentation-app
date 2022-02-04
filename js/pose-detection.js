096aec7d59dbbe9d68120de7164233cea20d956c
Ryo Suzuki <ryosuzuki@Ryos-Mac-mini.lan>
2022-02-04 07:57:41 -0700

    Show 3d object with hands

--- js/pose-detection.js
+++ js/pose-detection.js
@@ -32,6 +32,7 @@ AFRAME.registerComponent('webcam-pipeline', {
       function(stream) {
         let video = document.querySelector('video')
         video.srcObject = stream
+        video.style.transform = 'scaleX(-1)'
         video.onloadedmetadata = function(e) {
           video.play()
           video.loaded = true
@@ -41,6 +42,19 @@ AFRAME.registerComponent('webcam-pipeline', {
         console.log("The following error occured: " + err.name)
       }
     )
+
+    let leftEl = document.querySelector('#left-hand')
+    let rightEl = document.querySelector('#right-hand')
+    for (let i = 0; i < 21; i++) {
+      let sphere = document.createElement('a-sphere')
+      sphere.setAttribute('radius', 0.03)
+      let leftPointEl = sphere.cloneNode()
+      leftPointEl.setAttribute('id', `left-${i}`)
+      leftEl.appendChild(leftPointEl)
+      let rightPointEl = sphere.cloneNode()
+      rightPointEl.setAttribute('id', `right-${i}`)
+      rightEl.appendChild(rightPointEl)
+    }
   },
   tick: function() {
     let video = document.querySelector('video')
@@ -59,7 +73,8 @@ loadModel()
 async function predict(source) {
   let predictions = await window.model.estimateHands(source)
   if (predictions && predictions.length > 0) {
-    console.log(predictions)
+    // console.log(predictions)
+    showHands(predictions)
   } else {
     console.log('not found')
   }
@@ -74,6 +89,55 @@ function showHands(predictions) {
       let y = landmark[1]
       let z = landmark[2]
 
+      let raycaster = new THREE.Raycaster()
+      let pos2D = new THREE.Vector3(
+        ( x / window.innerWidth ) * 2 - 1,
+        ( y / window.innerHeight ) * 2 + 1
+      )
+      let sceneEl = document.querySelector('a-scene')
+      let camera = sceneEl.camera
+      raycaster.setFromCamera(pos2D, camera)
+
+      let background = document.querySelector('#background-plane')
+      let intersects = raycaster.intersectObjects([background.object3D])
+
+      debugger
+      for (let intersect of intersects) {
+        console.log(intersect.object)
+      }
+
+
+      /*
+      let projector = new THREE.Projector()
+      let pos = new THREE.Vector3(
+        x / window.innerWidth * 2 - 1,
+        -y / window.innerHeight * 2 + 1,
+        0.5
+      )
+      let camera = AFRAME.scenes[0].camera
+      projector.unprojectVector(pos, camera)
+      pos.sub(camera.position)
+      pos.normalize()
+
+      let rayCaster = new THREE.Raycaster(camera.position, pos)
+      let scale = window.innerWidth * 2
+      let rayDirection = new THREE.Vector3(
+        rayCaster.ray.direction.x * scale,
+        rayCaster.ray.direction.y * scale,
+        rayCaster.ray.direction.z * scale
+      )
+      let rayVector = new THREE.Vector3(
+        camera.position.x + rayDirection.x,
+        camera.position.y + rayDirection.y,
+        camera.position.z + rayDirection.z
+      )
+      */
+
+      // let pos = new THREE.Vector3(x, y, -1).unproject(camera)
+      // pos.z = -3
+      // let sphere = document.querySelector(`#left-${i}`)
+      // sphere.setAttribute('position', pos)
+      // console.log(pos)
     }
   }
 
