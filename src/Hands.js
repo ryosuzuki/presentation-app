3dd31a236efda053988e9bf8f219b1112b504fb3
Ryo Suzuki <ryosuzk@googlemail.com>
2022-02-14 14:33:53 -0700

    Refactor hand prediction code

--- src/Hands.js
+++ src/Hands.js
@@ -8,22 +8,14 @@ const THREE = AFRAME.THREE
 class Hands extends Component {
   constructor(props) {
     super(props)
+    window.Hands = this
+    this.App = window.App
   }
 
   componentDidMount() {
     this.loadModel()
 
-    for (let j = 0; j < 2; j++) {
-      let handEl = document.querySelector(`#hand-${j}`)
-      for (let i = 0; i < 21; i++) {
-        let sphereEl = document.createElement('a-sphere')
-        sphereEl.setAttribute('radius', 0.03)
-        sphereEl.setAttribute('id', `hand-${j}-${i}`)
-        handEl.appendChild(sphereEl)
-      }
-    }
-
-    AFRAME.registerComponent('hands', {
+    AFRAME.registerComponent('hands-update', {
       tick: () => {
         let video = document.querySelector('video')
         if (video.loaded) {
@@ -39,74 +31,63 @@ class Hands extends Component {
 
   async predict(source) {
     if (!this.model) return
-    this.predictions = await this.model.estimateHands(source)
-    this.showHands(this.predictions)
+    let predictions = await this.model.estimateHands(source)
+
+    predictions = predictions.map((hand) => {
+      hand.positions = hand.landmarks.map((landmark) => {
+        let pos = this.getPosition(landmark)
+        return pos
+      })
+      return hand
+    })
+
+    this.App.setState({ predictions: predictions })
   }
 
-  hideHands(handId) {
-    let handEl = document.querySelector(`#hand-${handId}`)
-    if (handEl.hidden) {
-      return false
+  getPosition(landmark) {
+    let x = landmark[0]
+    let y = landmark[1]
+    let z = landmark[2]
+
+    let raycaster = new THREE.Raycaster()
+    let pos2D = new THREE.Vector3(
+      ( x / window.innerWidth ) * 2 - 1,
+      - ( y / window.innerHeight ) * 2 + 1
+    )
+    let sceneEl = document.querySelector('a-scene')
+    let camera = sceneEl.camera
+    raycaster.setFromCamera(pos2D, camera)
+
+    let backgroundEl = document.querySelector('#background-plane')
+    let background = backgroundEl.object3DMap.mesh
+    let intersects = raycaster.intersectObject(background)
+    let pos = { x: 0, y: 0, z: 0 }
+    for (let intersect of intersects) {
+      pos = intersect.point
     }
-    handEl.hidden = true
-    for (let i = 0; i < 21; i++) {
-      let sphere = document.querySelector(`#hand-${handId}-${i}`)
-      sphere.setAttribute('position', '0 0 0')
-    }
-  }
-
-  showHands() {
-    if (!this.predictions || this.predictions.length === 0) {
-      // console.log('not found')
-      this.hideHands(0)
-      this.hideHands(1)
-      return false
-    }
-
-    for (let handId = 0; handId < 2; handId++) {
-      let hand = this.predictions[handId]
-      if (!hand) {
-        this.hideHands(handId)
-        continue
-      }
-
-      let handEl = document.querySelector(`#hand-${handId}`)
-      handEl.hidden = false
-      for (let i = 0; i < hand.landmarks.length; i++) {
-        let landmark = hand.landmarks[i]
-        let x = landmark[0]
-        let y = landmark[1]
-        let z = landmark[2]
-
-        let raycaster = new THREE.Raycaster()
-        let pos2D = new THREE.Vector3(
-          ( x / window.innerWidth ) * 2 - 1,
-          - ( y / window.innerHeight ) * 2 + 1
-        )
-        let sceneEl = document.querySelector('a-scene')
-        let camera = sceneEl.camera
-        raycaster.setFromCamera(pos2D, camera)
-
-        let backgroundEl = document.querySelector('#background-plane')
-        let background = backgroundEl.object3DMap.mesh
-        let intersects = raycaster.intersectObject(background)
-
-        for (let intersect of intersects) {
-          let pos = intersect.point
-          let sphere = document.querySelector(`#hand-${handId}-${i}`)
-          sphere.setAttribute('position', pos)
-        }
-
-      }
-    }
+    return pos
   }
 
 
   render() {
     return (
-      <a-entity id="hands" hands>
-        <a-entity id="hand-0"></a-entity>
-        <a-entity id="hand-1"></a-entity>
+      <a-entity id="hands" hands-update>
+        { this.App.state.predictions.map((hand, i) => {
+          return (
+            <a-entity id={`hand-${i}`} key={ i }>
+              { hand.positions.map((position, j) => {
+                return (
+                  <a-sphere
+                    id={`hand-${i}-${j}`}
+                    key={ j }
+                    radius="0.03"
+                    position={ `${position.x} ${position.y} ${position.z}` }
+                  ></a-sphere>
+                )
+              }) }
+            </a-entity>
+          )
+        })}
       </a-entity>
     )
   }
